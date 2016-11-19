9e85207a4b12a0cb8f0f4f20769adfe22e0c5035
yuyingliu331 <liuyuying90@live.cn>
2016-11-19 16:06:06 -0500

    updated with question 3

--- Palindrome Number.js
+++ Palindrome Number.js
@@ -1,12 +1,22 @@
 
 var isPalindrome = function(x) {
-  if(x.toString() === 1){
+  x = x.toString();
+  if(x.length <= 1){
     return true;
   }
-  else if(x.toString() === 2){
-    if(x.charAt(0) === '-') return true;
+  else if(x.length === 2){
+
+    if(x.charAt(0) === '-') return false;
     else if (x.charAt(0) === x.charAt(1)) return true;
     else return false;
+  }
+  else{
+    if(x.charAt(0) === '-') return false;
+    for(var i = 0; i < x.length /2; i++){
+    console.log(x.charAt(i));
+      if(x.charAt(i) != x.charAt(x.length -1-i)) return false;
+    }
+    return true;
   }
 
 
