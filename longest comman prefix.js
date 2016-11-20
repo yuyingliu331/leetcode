7e6415e3c9e09c26fcd61c9bb9fd0c8c87f158f7
yuyingliu331 <liuyuying90@live.cn>
2016-11-20 11:07:23 -0500

    submit solution part 6

--- longest comman prefix.js
+++ longest comman prefix.js
@@ -1,4 +1,15 @@
 var longestCommonPrefix = function(strs) {
   if(strs.length === 0 || strs === null) return "";
-
+  var len = strs.reduce(function(num, str){ //num: accumulator, str: current Value
+    return Math.min(num, str.length);
+  }, Infinity);
+  if(len === 0) return "";
+  for(var i = 0; i < len; i ++){
+    var char = strs[0].charAt(i);
+    for(var j = 1; j < strs.length; j++){
+      var char2 = strs[j].charAt(i);
+      if(char != char2) return strs[0].substring(0, i);
+    }
+  }
+  return strs[0].substring(0, len);
 };
---
+++ valid parentheses.js
@@ -0,0 +1,33 @@
+var isValid = function(s) {
+  var stack = [];
+  s = s.split("");
+  if(s.length === 0) return true;
+  for(var i = 0; i < s.length; i++){
+    if(stack.length === 0) stack.push(s[i]);
+    else{
+       if(isMatch(stack[stack.length -1], s[i])) stack.pop();
+       else{
+        if(!isValidChar(s[i]) || stack.length > (s.length - i - 2)) return false;
+        stack.push(s[i]);
+       }
+
+    }
+  }
+  return stack.length === 0;
+};
+
+function isMatch(str1, str2){
+  var obj = {
+    '{' : "}",
+    '(' : ")",
+    '[' : "]"
+  };
+  if(obj[str1] === str2) return true;
+  else return false;
+
+}
+
+function isValidChar(str){
+  if(str === '{' || str === '(' || str === '[' ) return true;
+  else return false;
+}
